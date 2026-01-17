2b937e5b00cea32e43a33a41eec0599932489ab1
Developer <developer@example.com>
2026-01-17 14:27:35 +0530

    Update repository

--- components/AdminDashboard.tsx
+++
@@ -1,6 +0,0 @@
-
-// This file is deprecated and replaced by SuperAdminDashboard.tsx
-// Re-exporting for compatibility if imported elsewhere erroneously.
-
-import SuperAdminDashboard from './SuperAdminDashboard';
-export default SuperAdminDashboard;
--- components/RoleSelector.tsx
+++
@@ -1,3 +0,0 @@
-
-import React from 'react';
-import { Role } from '../types';
